4b06e0dc540ab184b6ca5f124b9f254c557d2581
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-19 21:03:21 +0200

    bug TODO

--- springbokgallery/editor_plugin.js
+++ springbokgallery/editor_plugin.js
@@ -20,7 +20,10 @@
 
 			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
 			ed.addCommand('springbokGallery', function() {
-				gallery.setOnSelectImage(function(idImage,image){ed.execCommand('mceInsertContent', false,'<img src="'+webdir+'files/cms_images/'+idImage+'.jpg" width="'+image.width+'" height="'+image.height+'" />');gallery.close();});
+				gallery.setOnSelectImage(function(idImage,image){
+					ed.execCommand('mceInsertContent', false,'<img src="'+basedir+'web/files/cms_images/'+idImage+'.jpg"'/* width="'+image.width+'" height="'+image.height+'"*/+'/>');
+					gallery.close();
+				});
 				gallery.load(true);
 			});
 
