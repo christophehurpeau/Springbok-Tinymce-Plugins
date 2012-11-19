1f8ab5d3d31b1daffcfaaff588a43da5663cb030
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-11-19 11:39:07 +0100

    clean

--- springbokclean/editor_plugin.js
+++ springbokclean/editor_plugin.js
@@ -17,34 +17,41 @@
 		cleanHTML:function(content){
 			if(content.sHas("_____")||content.sHas("-----")) alert("Utilisez des <hr> !")
 			// remove invalid parent paragraphs when pasting HTML and/or switching to the HTML editor and back
+			//console.log(content);
 			content = content.replace(/<\/p>\s*/g,"</p>\n")
 					.replace(/<p>\s*<(p|div|ul|ol|dl|table|blockquote|h[1-6]|fieldset|pre|address)( [^>]*)?>/gi, '<$1$2>')
 					.replace(/<\/(p|div|ul|ol|dl|table|blockquote|h[1-6]|fieldset|pre|address)>\s*<\/p>/gi, '</$1>')
 					.replace(/style="([^"]+)"/gi,function(str,p1,offset,s){ return 'style="'+p1.replace(/\s*([;|:])\s*/g,'$1')+'"'; })
+					.replace(/<span>([^<]*)<\/span>/g,'$1');
 				;
 			var i=4;
+			//console.log(content);
 			while(i-- > 0){
 				content = content.replace(/<span[^>]*>\s*<\/span>/g,'');
 				content = content.replace(/<p[^>]*>\s*<\/p>\s*<p>/g,'<p class="mt20">');
 			}
+			//console.log(content);
 			i=4;
 			while(i-- > 0){
 				content=content.replace(/<span([^>]*)>(\s*)<span([^>]*)>(.*)<\/span>(\s*)<\/span>/,function(str,p1,p2,p3,p4,p5,offset,s){
-					var attributes={},attrsMatches,style,m;
+					var attributes={},attrsMatches,style,m,hasAttributes=false;
 					$.each([p1,p3],function(i,attrs){
 						attrsMatches=attrs.match(/([a-zA-Z]+)="([^"]+)"/g);
-						if(attrsMatches) $.each(attrsMatches,function(k,attr){
-							m=/^([a-zA-Z]+)="([^"]+)"$/.exec(attr);
-							if(m[1]==='style'){
-								style=m[2].trim().rtrim(';');
-								attributes[m[1]] ? attributes[m[1]]+=';'+style : attributes[m[1]]=style;
-							}else attributes[m[1]]=m[2];
-						});
+						if(attrsMatches){
+							hasAttributes=true;
+							$.each(attrsMatches,function(k,attr){
+								m=/^([a-zA-Z]+)="([^"]+)"$/.exec(attr);
+								if(m[1]==='style'){
+									style=m[2].trim().rtrim(';');
+									attributes[m[1]] ? attributes[m[1]]+=';'+style : attributes[m[1]]=style;
+								}else attributes[m[1]]=m[2];
+							});
+						}
 					});
-					
-					return '<span'+$.map(attributes,function(v,k){return ' '+k+'="'+v+'"'})+'>'+p2+p4+p5+'</span>';
+					return hasAttributes ? ('<span'+$.map(attributes,function(v,k){return ' '+k+'="'+v+'"'})+'>'+p2+p4+p5+'</span>' ) : p2+p4+p5;
 				});
 			}
+			//console.log(content);
 			return content;
 		},
 
